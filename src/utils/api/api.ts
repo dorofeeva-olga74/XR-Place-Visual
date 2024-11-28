ea36324e5ab3df102ce3fb21133418fe4ee02628
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-11-28 11:11:26 +0300

    delete comments

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -3,27 +3,12 @@ import { Advantage } from './apiTypes';
 
 interface ApiInterface {
   getAdvantages: (lang) => Promise<{ data: Advantage[] }>;
-  /* addMeal: (restaurantId: number, mealId: number, fetures: Feature[] | never[]) => Promise<{ data: Basket }>;
-  deleteMeal: (restaurantId: number, mealId: number, fetures: Feature[] | never[]) => Promise<{ data: Basket }>;
-  emptyBasket: () => Promise<{ data: Basket }>; */
 }
 
 class Api implements ApiInterface {
   async getAdvantages(lang): Promise<{ data: Advantage[] }> {
     return fetchData(lang, 'advantages/');
   }
-  /* async addMeal(restaurant_id: number, meal_id: number, features: Feature[] | never[]): Promise<{ data: Basket }> {
-      return handleFetch('api/basket/add_meal', { method: 'POST', data: { restaurant_id, meal_id, features } });
-  }
-  async deleteMeal(restaurant_id: number, meal_id: number, features: Feature[] | never[]): Promise<{ data: Basket }> {
-      return handleFetch('api/basket/delete_meal', { method: 'POST', data: { restaurant_id, meal_id, features } });
-  }
-  async emptyBasket(): Promise<{ data: Basket }> {
-      return handleFetch('api/basket/empty', { method: 'POST' });
-  }
-  async placeOrder(userId: string, restaurantId: number): Promise<OrderState> {
-      return handleFetch('api/orders', { method: 'POST', data: { restaurantId, userId } });
-  } */
 }
 
 export const api = new Api();
