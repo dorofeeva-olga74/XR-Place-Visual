2c9083119bdfe20c6f8a750998ec9c7c6a91d0ae
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-04 16:38:04 +0300

    change class selectors to kebab-case

--- src/components/Claim/Claim.tsx
+++ src/components/Claim/Claim.tsx
@@ -3,22 +3,22 @@ import { inputs } from '../../utils/consts';
 import IntroButton from '../Ui/IntroButton/IntroButton';
 
 function Input(input) {
-  return <input className={styles.claim__input} name={input.name} placeholder={input.placeholder} required={input.required}></input>;
+  return <input className={styles.claim - input} name={input.name} placeholder={input.placeholder} required={input.required}></input>;
 }
 
 function Claim() {
   return (
-    <div className={styles.claim__container}>
+    <div className={styles.claim - container}>
       <form>
-        <h2 className={styles.claim__title}>Назначить демо</h2>
+        <h2 className={styles.claim - title}>Назначить демо</h2>
         {inputs.map((input, index) => {
           return <Input key={`${input.name} - ${index}`} input={input} />;
         })}
-        <div className={styles.claim__button}>
+        <div className={styles.claim - button}>
           <IntroButton>Отправить заявку</IntroButton>
         </div>
-        <small className={styles.claim__annotation}>
-          Нажимая на кнопку, вы принимаете <span className={styles.claim__annotation}>условия использования</span> и <span className={styles.claim__annotation}>политику конфиденциальности</span>
+        <small className={styles.claim - annotation}>
+          Нажимая на кнопку, вы принимаете <span className={styles.claim - annotation}>условия использования</span> и <span className={styles.claim - annotation}>политику конфиденциальности</span>
         </small>
       </form>
     </div>
