3d8c0948a0a01fb9706c0d766084ba94b7a970b0
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-07 22:03:39 +0300

    create progress bar for Widget's List

--- src/components/Widget/List/List.tsx
+++ src/components/Widget/List/List.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import { forwardRef, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './List.module.scss';
+import { motion, useScroll } from 'motion/react';
 
-const ListItem: React.FC<{ text: string }> = ({ text }) => {
-  return <li className={styles['list-item']}>{text}</li>;
-};
+const ListItem = forwardRef(({ text }, ref) => {
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end end'],
+  });
+  return (
+    <li ref={ref} className={styles['list-item']}>
+      <p>{text}</p>
+      <motion.div className={styles['progress-bar']} style={{ scaleX: scrollYProgress }}></motion.div>
+    </li>
+  );
+});
 
 const List: React.FC = () => {
+  const ref = useRef(null);
   const { t } = useTranslation();
   const data: string[] = t('components.widget.list.items', { returnObjects: true }) as unknown as string[];
   return (
-    <ul className={styles['list']}>
+    <ul ref={ref} className={styles['list']}>
       {data.map((item, index) => {
-        return <ListItem key={`${item}-${index}`} text={item} />;
+        return <ListItem key={`${item}-${index}`} text={item} ref={ref} />;
       })}
     </ul>
   );
