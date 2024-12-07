c74b43ce683d4cd65d293dfd8d6f17582f2da571
dorofeeva-olga74 <dorofeeva.olga74@mail.ru>
2024-12-07 18:57:12 +0900

    fix Metrics

--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -26,26 +26,3 @@ const Metrics: React.FC = () => {
 };
 
 export default Metrics;
-
-// return (
-//   <div className={styles['metrics']}>
-//       <h2 className={styles['metrics-title']}>{t('components.metrics.title')}</h2>
-//       <div className={styles['metrics-details']}>
-//         <div className={styles['metrics-set']}>
-//           <p className={styles['metrics-text']}>{t('components.metrics.first')}</p>
-//           <span className={styles['metrics-span']}>15%</span>
-//         </div>
-//         <div className={styles['metrics-set']}>
-//           <p className={styles['metrics-text']}>{t('components.metrics.second')}</p>
-//           <span className={styles['metrics-span']}>20%</span>
-//         </div>
-//         <div className={styles['metrics-set']}>
-//           <p className={styles['metrics-text']}>{t('components.metrics.third')}</p>
-//           <span className={styles['metrics-span']}>60%</span>
-//         </div>
-//       </div>
-//   </div>
-// );
-// };
-
-// export default Metrics;
