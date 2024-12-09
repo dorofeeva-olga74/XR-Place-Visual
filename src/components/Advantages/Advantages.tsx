d384242c0b2d9c55227e1cabc5c006a6b19a2dbd
dorofeeva-olga74 <dorofeeva.olga74@mail.ru>
2024-12-09 19:05:32 +0900

    fix Advantages

--- src/components/Advantages/Advantages.tsx
+++ src/components/Advantages/Advantages.tsx
@@ -7,27 +7,26 @@ const Advantages: React.FC = () => {
   const { t } = useTranslation();
   const advantages = useAdvantages('RU');
 
-  if (!advantages.isSuccess) {
+  if (advantages.isError) {
     return null;
   }
 
   return (
-    <div className={styles['advantages']}>
-      <h2 className={styles['advantages__title']}>{t('components.advantages.title')}</h2>
-      <div className={styles['advantages__details']}>
+    <div className={styles.advantages}>
+      <h2 className={styles.advantages__title}>{t('components.advantages.title')}</h2>
+      <div className={styles.advantages__details}>
         {advantages.isSuccess &&
           advantages.data.slice(0, 3).map((item, index) => {
             return (
-              <div className={styles['advantages__card']} key={`${item.id}`}>
-                <div className={styles['advantages__circle-container']}>
-                  <div className={styles['advantages__circle']}></div>
-                  {index >= 1 && <div className={`${styles['advantages__circle']} ${styles['advantages__circle-second']}`}></div>}
-                  {index === 2 && <div className={`${styles['advantages__circle']} ${styles['advantages__circle-second']} ${styles['advantages__circle-third']}`}></div>}
+              <div className={styles.advantages__card} key={`${item.id}`}>
+                <div className={styles.advantages__circleContainer}>
+                  <div className={styles.advantages__circle}></div>
+                  {index >= 1 && <div className={`${styles.advantages__circle} ${styles.advantages__circleSecond}`}></div>}
+                  {index === 2 && <div className={`${styles.advantages__circle} ${styles.advantages__circleSecond} ${styles.advantages__circleThird}`}></div>}
                 </div>
-                <h3 className={styles['advantages__card-title']}>{item.title}</h3>
-                <ul className={styles['advantages__text-set']}>
-                  <li className={styles['advantages__text']}>{item.description}</li>
-                  <li className={styles['advantages__text']}>{item.description}</li>
+                <h3 className={styles.advantages__cardTitle}>{item.title}</h3>
+                <ul className={styles.advantages__textSet}>
+                  <li className={styles.advantages__text}>{item.description}</li>
                 </ul>
               </div>
             );
