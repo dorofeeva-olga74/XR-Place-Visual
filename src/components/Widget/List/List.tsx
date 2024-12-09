f64a74f0de9b3fcd7ddf10d4bef33f21bfeb78a3
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-09 19:26:47 +0300

    adjust offsets in useScroll

--- src/components/Widget/List/List.tsx
+++ src/components/Widget/List/List.tsx
@@ -1,29 +1,29 @@
-import { forwardRef, useRef } from 'react';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './List.module.scss';
-import { motion, useScroll } from 'motion/react';
+import { motion, MotionValue, useScroll } from 'motion/react';
 
-const ListItem = forwardRef(({ text }: { text: string }, ref) => {
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'end end'],
-  });
+const ListItem = ({ text, progress }: { text: string; progress: MotionValue }) => {
   return (
-    <li ref={ref} className={styles['list-item']}>
+    <li className={styles['list-item']}>
       <p>{text}</p>
-      <motion.div className={styles['progress-bar']} style={{ scaleX: scrollYProgress }}></motion.div>
+      <motion.div className={styles['progress-bar']} style={{ scaleX: progress }}></motion.div>
     </li>
   );
-});
+};
 
 const List: React.FC = () => {
   const ref = useRef(null);
   const { t } = useTranslation();
   const data: string[] = t('components.widget.list.items', { returnObjects: true }) as unknown as string[];
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start 0.9', 'end 0.7'],
+  });
   return (
     <ul ref={ref} className={styles['list']}>
       {data.map((item, index) => {
-        return <ListItem key={`${item}-${index}`} text={item} ref={ref} />;
+        return <ListItem key={`${item}-${index}`} text={item} progress={scrollYProgress} />;
       })}
     </ul>
   );
