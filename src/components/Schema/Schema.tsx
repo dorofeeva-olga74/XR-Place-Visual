ece10302bbbe7fdbb8f7a36cbf751b7ec02875e5
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-10 12:18:29 +0300

    add responsive styles

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -2,15 +2,26 @@ import styles from './Schema.module.scss';
 import image from './../../vendor/images/Schema.png';
 import { useSchema } from '../../utils/hooks/useSchema';
 import { SchemaStage } from '../../utils/api/apiTypes';
+import useWindowWidth from '../../utils/hooks/useWindowWidth';
+
+function Description() {
+  return (
+    <p className={styles.schema__description}>
+      <span className={styles.schema__description_span}>Выбирая нас, </span>
+      вы получаете гарантию качества, внимание к деталям и соблюдение сроков
+    </p>
+  );
+}
 
 function Schema() {
   const schema = useSchema('RU');
+  const { width } = useWindowWidth();
 
   return (
     <section className={styles.schema__container}>
       <div className={styles.schema__overview}>
         <h2 className={styles.schema__title}>СХЕМА РАБОТЫ</h2>
-        <p className={styles.schema__description}>Выбирая нас, вы получаете гарантию качества, внимание к деталям и соблюдение сроков</p>
+        {width >= 360 && <Description />}
         <div className={styles.schema__image_container}>
           <img className={styles.schema__image} src={image}></img>
         </div>
@@ -27,6 +38,7 @@ function Schema() {
             );
           })}
       </ul>
+      {width <= 360 && <Description />}
     </section>
   );
 }
