37681789ceda313903bd742d5a2febdaf26a1485
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-13 00:58:40 +0300

    use motion.img

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,46 +1,17 @@
 import React, { useRef } from 'react';
 import styles from './Widget.module.scss';
-import GlowingCircle from '../Ui/GlowingCircle/GlowingCircle';
 import FeaturesTextSection from './FeaturesTextSection/FeaturesTextSection';
 import FeaturesWidgetSection from './FeaturesWidgetSection/FeaturesWidgetSection';
 import { gridElements } from '../../utils/consts';
 import { motion, useScroll, MotionValue } from 'motion/react';
+import horizontalLine from './../../vendor/images/horizontal-line.png';
+import virticalLine from './../../vendor/images/vertical-line.png';
 
-const RayAnimationX = {
-  hidden: {
-    x: 0,
-    opacity: 0,
-  },
-  visible: (custom: number) => ({
-    x: 100,
-    opacity: 1,
-    transition: { delay: custom * 0.7 },
-  }),
-  visualViewport: {
-    amount: 0.2,
-    once: true,
-  },
-};
-const RayAnimationY = {
-  hidden: {
-    y: 0,
-    opacity: 0,
-  },
-  visible: (custom: number) => ({
-    y: 50,
-    opacity: 1,
-    transition: { delay: custom * 0.7 },
-  }),
-  visualViewport: {
-    amount: 0.2,
-    once: true,
-  },
-};
 const RayX = ({ progress }: { progress: MotionValue }) => {
-  return <motion.div custom={1} variants={RayAnimationX} initial="hidden" animate="visible" className={styles['progress-ray-x']} style={{ scaleX: progress }} />;
+  return <motion.img src={horizontalLine} className={styles['progress-ray-x']} style={{ scaleX: progress }} />;
 };
 const RayY = ({ progress }: { progress: MotionValue }) => {
-  return <motion.div custom={2} variants={RayAnimationY} initial="hidden" animate="visible" className={styles['progress-bar-y']} style={{ scaleY: progress }} />;
+  return <motion.img src={virticalLine} className={styles['progress-bar-y']} style={{ scaleY: progress }} />;
 };
 
 const Widget: React.FC = () => {
@@ -48,18 +19,18 @@ const Widget: React.FC = () => {
 
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ['start end', 'end start'],
+    offset: ['start 0.9', 'end 0.7'],
   });
   return (
-    <section className={styles['widget']} ref={ref}>
-      <div className={styles['grid-background']}>
+    <section className={styles['widget']}>
+      <div ref={ref} className={styles['grid-background']}>
         {gridElements.map((item, index) => (
           <div key={`${item} - ${index}`} className={styles['grid-item']} />
         ))}
       </div>
       <motion.div initial={'hidden'} whileInView={'visible'} className={styles['widget-text-container']}>
         <div className={styles['widget-circle-container']}>
-          <GlowingCircle />
+          <div className={styles.widget_circle} />
           <RayX progress={scrollYProgress} />
           <RayY progress={scrollYProgress} />
         </div>
