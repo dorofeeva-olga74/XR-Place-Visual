634eac380a9e0b3ebd72e65d616425011bbd71a2
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-12-12 15:59:13 +0300

    Revert "ManifestationText"

--- src/components/Widget/FeaturesTextSection/FeaturesTextSection.tsx
+++ src/components/Widget/FeaturesTextSection/FeaturesTextSection.tsx
@@ -1,38 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './FeaturesTextSection.module.scss';
 
 const FeaturesTextSection: React.FC = () => {
   const { t } = useTranslation();
-  const ref = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const element = ref.current;
-
-    if (!element) return;
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          element.classList.add(styles.visible);
-          element.classList.remove(styles.hidden);
-        } else {
-          element.classList.add(styles.hidden);
-          element.classList.remove(styles.visible);
-        }
-      },
-      { threshold: 0.1 }
-    );
-    observer.observe(element);
-    return () => {
-      observer.unobserve(element);
-    };
-  }, []);
-
   return (
-    <div className={styles.shimmerTextSet} ref={ref}>
-      <p className={styles.shimmerText}>
+    <div className={styles['shimmer-text-set']}>
+      <p className={styles['shimmer-text']}>
         {t('components.widget.featuresTextSection.p')}
-        <span className={styles.shimmerSpan}>{t('components.widget.featuresTextSection.span')}</span>
+        <span className={styles['shimmer-span']}>{t('components.widget.featuresTextSection.span')}</span>
         {t('components.widget.featuresTextSection.p2')}
       </p>
     </div>
