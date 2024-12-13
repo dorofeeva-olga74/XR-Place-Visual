ae6e2268719725dff72e66ed7b5ecee595cc03a3
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-12-13 22:29:29 +0300

    fix ts config and fix ts errors

--- src/utils/hooks/useSchema.ts
+++ src/utils/hooks/useSchema.ts
@@ -1,5 +1,6 @@
 import { skipToken, useQuery } from '@tanstack/react-query';
 import { api } from '../api/api';
+import { Lang } from '../api/apiTypes';
 
 export const useSchema = (lang: Lang, enabled: boolean) => {
   return useQuery({
